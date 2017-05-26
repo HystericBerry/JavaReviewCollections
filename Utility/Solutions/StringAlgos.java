f73259c51196d51c18022baf373d98cbd8e060d2
HystericBerry <pkim7662@gmail.com>
2017-05-26 00:19:07 -0700

    Updated String Algorithms to include Permutation.

--- Utility/Solutions/StringAlgos.java
+++ Utility/Solutions/StringAlgos.java
@@ -1,7 +1,72 @@
 package Solutions;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class StringAlgos
 {
+	/**
+	 * My fast implementation of Permutation generator
+	 * 
+	 * @param s
+	 * @return
+	 */
+	public static Set<String> permutation( String s )
+	{
+		Set<String> perms = new HashSet<String>();
+		perms.add(s);
+		permHelper( perms, s, 0, s.length() );
+		
+		return perms;
+	}
+	
+	/**
+	 * 
+	 * @param set
+	 * @param s
+	 * @param start
+	 * @param end
+	 */
+	private static void permHelper( Set<String> set, String s, int start, int end )
+	{
+		if( start >= end ) return;
+		
+		for( int i = start+1; i < end; ++i )
+		{
+			char[] c = s.toCharArray();
+			swap(c, start, i);
+			
+			String s2 = new String(c);
+			set.add(s2);
+			permHelper( set, s2, start+1, end );
+		}
+		
+		permHelper( set, s, start+1, end );
+	}
+	
+	/**
+	 * Permutation Solution found on Princeton to compare answers in TDD Unit Testing.
+	 * 
+	 * @param s
+	 * @return
+	 */
+	public  static Set<String> permutationSoln(String s)
+	{
+		Set<String> perms = new HashSet<String>();
+		permSolnHelper(perms, "", s);
+		
+		return perms;
+	}
+    private static void permSolnHelper(Set<String> perms, String prefix, String s) {
+        int n = s.length();
+        if( n == 0 ) perms.add(prefix);
+        else {
+            for (int i = 0; i < n; i++)
+            	permSolnHelper(perms, prefix + s.charAt(i), s.substring(0, i) + s.substring(i+1, n));
+        }
+
+    }
+	
 	public static boolean isPalinIter( String s )
 	{
 		if( s == null )
@@ -35,5 +100,12 @@ public class StringAlgos
 			return true;
 		
 		return (c[start] == c[end]) && palinHelper(c, start+1, end-1);
+	}
+	
+	private static void swap( char[] c, int a, int b )
+	{
+		char temp = c[a]; // store a
+		c[a] = c[b]; // overwrite a with b
+		c[b] = temp; // overwrite b with temp
 	}
 }
